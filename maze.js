67819bd5b2f125fbe48a461fc5ca458f1ea8fc5f
davdbryson456 <32532033+davdbryson456@users.noreply.github.com>
2017-10-20 18:14:12 -0500

    Question3
    Question3

--- maze.js
+++ maze.js
@@ -3,7 +3,7 @@ window.onload = function(){
 
 	doEverything();	
 }
-	
+var stop=false;	
 
 function youLose() {
     document.getElementById("boundary1").classList.add("youlose");
@@ -13,12 +13,14 @@ function youLose() {
 
 
 function youlose2(){
+	stop=true;
 	 var loser = document.getElementsByClassName("boundary");
 	 var x;
 
 		 for(x = 0; x < loser.length; x++) {
 		  loser[x].classList.add("youlose");
 	 	}
+	 	//document.getElementById("status").innerHTML = "YOU LOSE";
 };
 
 function doEverything(){
@@ -27,7 +29,20 @@ function doEverything(){
 	 var i;
 
 	 for(i = 0; i < allbound.length; i++ ) {
+
 	  	allbound[i].addEventListener("mouseover", youlose2);
+	  	
 	 }
+	 end=document.getElementById("end").addEventListener("mouseover", youWin);
 };
 
+function youWin(){
+	if (stop==true){
+
+	}else{
+
+		window.alert("YOU WIN");
+	}
+
+	
+};
